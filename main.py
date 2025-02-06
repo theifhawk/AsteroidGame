cd658f83ba2e1aeb7e4fad8efe1df36da98df274
Theifhawk <wchen291@gmail.com>
2025-02-05 19:29:23 -0500

    Added Player Sprite and rotation

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting asteroids!")
     pygame.init()
+    Gameclock = pygame.time.Clock()
+    dt = 0
+    player_1 = Player(SCREEN_WIDTH/2 , SCREEN_HEIGHT/2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player_1.update(dt)
         screen.fill(000)
+        player_1.draw(screen)
         pygame.display.flip()
+        dt = (Gameclock.tick(60))/1000
 
 
 
